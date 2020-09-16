9dddf57340333ebd248dbaf8153241330feda73d
Atharva Deosthale <atharva.deosthale17@gmail.com>
2020-09-16 14:01:26 +0530

    UI Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { MonetizationOn, LocalOffer } from "@material-ui/icons"; // importing the icons to make the textboxes look cool
+import { ToastContainer, toast, Flip } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
@@ -6,17 +9,29 @@ function App() {
   const [percent, setPercent] = useState("");
   const [tip, setTip] = useState("");
 
-  useEffect(() => {}, []);
-
   const calculateTip = (e) => {
     e.preventDefault();
+
+    // adding checks to see if anything is entered
+    if (!amount || !percent) {
+      // return to stop execution of function immediately
+      return toast.error("Please enter all the fields!", {
+        transition: Flip,
+        hideProgressBar: true,
+      });
+    }
+
+    toast.success("Request sent!", {
+      hideProgressBar: true,
+      transition: Flip,
+    });
 
     const data = {
       amount,
       tip: percent,
     };
 
-    const response = fetch("http://localhost:9000/api/v1/calculatetip", {
+    fetch("http://localhost:9000/api/v1/calculatetip", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -30,19 +45,33 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <input
-        type="text"
-        value={percent}
-        onChange={(e) => setPercent(e.target.value)}
-      />
-      <button onClick={calculateTip}>Send</button>
-      <h1>{tip}</h1>
+    // BEM Convention
+    <div className="app">
+      <div className="app__container">
+        <div className="app__inputContainer">
+          <MonetizationOn className="app__inputIcon" />
+          <input
+            type="text"
+            value={amount}
+            placeholder="Amount"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="app__inputContainer">
+          <LocalOffer className="app__inputIcon" />
+          <input
+            type="text"
+            value={percent}
+            placeholder="Percentage"
+            onChange={(e) => setPercent(e.target.value)}
+          />
+        </div>
+        <button onClick={calculateTip} className="app__inputButton">
+          Send
+        </button>
+        <h1>{tip}</h1>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
